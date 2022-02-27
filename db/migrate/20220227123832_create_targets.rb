53fcd90de264cf98de6b0e54e49eeba5c41f4e3d
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-02-27 21:45:38 +0900

    rails db:migrate実行

--- db/migrate/20220227123832_create_targets.rb
+++ db/migrate/20220227123832_create_targets.rb
@@ -1,7 +1,10 @@
 class CreateTargets < ActiveRecord::Migration[6.0]
   def change
     create_table :targets do |t|
-
+      t.integer :max_hp, null: false
+      t.integer :max_mp, null: false
+      t.integer :current_hp, null: false
+      t.integer :current_mp, null: false
       t.timestamps
     end
   end
