a14743f87d7de13d6aec8d8246e5e69cee76a710
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-02-28 11:30:49 +0900

    createアクション定義

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,5 +1,5 @@
 class TargetsController < ApplicationController
-  before_action :move_to_new
+  before_action :move_to_new, only: [:index]
   def index
   end
 
@@ -7,10 +7,29 @@ class TargetsController < ApplicationController
     @target = Target.new
   end
 
+  def create
+    @target = Target.new(target_params)
+    target_params_addition
+    if @target.save
+      redirect_to new_target_budget_path(@target)
+    else
+      render :new
+    end
+  end
+
   private
   def move_to_new
     if @target == nil
       render new_target_path
     end
+  end
+
+  def target_params
+    params.permit(:max_hp, :max_mp)
+  end
+
+  def target_params_addition
+    @target.current_hp = @target.max_hp
+    @target.current_mp = 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'targets#index'
-  resources :targets, only: [:index, :new] do
+  resources :targets, only: [:index, :new, :create] do
     resources :budgets, only: [:new]
   end
 end
