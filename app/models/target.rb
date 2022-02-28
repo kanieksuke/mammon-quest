2108f55915535054fce50f1e622ccb41baf69e25
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-02-28 16:02:20 +0900

    targetモデルの単体テスト実施

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,7 +2,7 @@ class Target < ApplicationRecord
   with_options presence: true do
     validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
     validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
-    validates :current_amount
-    validates :current_date
+    validates :current_amount, numericality: {only_integer: true}
+    validates :current_date, numericality: {only_integer: true}
   end
 end
