014c5ec3b747590d3cc3a148f26a97cc813f6966
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-02-28 09:40:03 +0900

    目標を設定していない場合、目標設定画面に遷移

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,7 +1,16 @@
 class TargetsController < ApplicationController
+  before_action :move_to_new
   def index
   end
 
   def new
     @target = Target.new
+  end
+
+  private
+  def move_to_new
+    if @target == nil
+      render new_target_path
+    end
+  end
 end
