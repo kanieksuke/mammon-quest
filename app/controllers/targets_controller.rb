785f25ddbbf31c49d281a78aec1af604436c339d
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-02 21:17:16 +0900

    targetsとbudgetsのクリエイトアクションを統合

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,4 +1,5 @@
 class TargetsController < ApplicationController
+  before_action :authenticate_user!
   before_action :move_to_new, only: [:index]
   def index
     @user = User.find(params[:id])
@@ -9,8 +10,9 @@ class TargetsController < ApplicationController
 
   def create
     @target = Target.new(target_params)
-    target_params_addition
-    if @target.save
+    @budget = Budget.new(budget_params)
+    target_budget_params_addition
+    if @target.save && @budget.save
       redirect_to new_target_budget_path(@target)
     else
       render :new
@@ -28,8 +30,19 @@ class TargetsController < ApplicationController
     params.require(:target).permit(:target_amount, :target_date).merge(user_id: current_user.id)
   end
 
-  def target_params_addition
+  def budget_params
+    params.require(:budget).permit(:income, :fixed_cost).merge(user_id: current_user.id, target_id: params[:target_id])
+  end
+
+  def target_budget_params_addition
     @target.current_amount = @target.target_amount
     @target.current_date = 0
+    d = Date.new(Time.now.year, Time.now.month, -1).day
+    if @budget.income =! nil && @budget.fixed_cost =! nil
+      @budget.attack = (@budget.income - @budget.fixed_cost) / d
+    else
+      @budget.attack = 0
+    end
+    @budget.resist = 0
   end
 end
