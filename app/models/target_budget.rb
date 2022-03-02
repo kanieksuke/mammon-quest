1ab7637b4231210d2038ec6e0736b45e775b859a
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-02 21:27:16 +0900

    保存するカラムを指定

--- app/models/target_budget.rb
+++ app/models/target_budget.rb
@@ -1,2 +1,4 @@
 class TargetBudget
+  include ActiveModel::Model
+  attr_accessor :target_amount, :target_date, :current_amount, :current_date, :income, :fixed_cost, :attack, :resist, :user_id, :target_id
 end
