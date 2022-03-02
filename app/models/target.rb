e727702138755462fd3665dfdacdb2e162f5d513
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-02 21:32:49 +0900

    バリデーション設定

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,10 +1,4 @@
 class Target < ApplicationRecord
-  with_options presence: true do
-    validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
-    validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
-    validates :current_amount, numericality: {only_integer: true}
-    validates :current_date, numericality: {only_integer: true}
-  end
   belongs_to :user
   has_many :budgets
 end
--- app/models/target_budget.rb
+++ app/models/target_budget.rb
@@ -1,4 +1,15 @@
 class TargetBudget
   include ActiveModel::Model
   attr_accessor :target_amount, :target_date, :current_amount, :current_date, :income, :fixed_cost, :attack, :resist, :user_id, :target_id
+
+  with_options presence: true do
+    validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :current_amount, numericality: {only_integer: true}
+    validates :current_date, numericality: {only_integer: true}
+    validates :income, numericality: {only_integer: true}
+    validates :fixed_cost, numericality: {only_integer: true}
+    validates :attack, numericality: {only_integer: true}
+    validates :resist, numericality: {only_integer: true}
+  end
 end
