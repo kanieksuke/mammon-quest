8596909f57512da6915fa9a7f16fce63610e3825
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-02 19:49:42 +0900

    createアクション定義

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,4 +2,26 @@ class BudgetsController < ApplicationController
   def new
     @budget = Budget.new
   end
+
+  def create
+    @budget = Budget.new(budget_params)
+    budget_params_addition
+    if @budget.save
+      redirect_to root_path(@budget)
+    else
+      render :new
+    end
+  end
+
+  private
+  def budget_params
+    params.require(:budget).permit(:incum, :fixed_cost).merge(user_id: current_user.id, target_id: params[:target_id])
+  end
+
+  def budget_params_addition
+    d = Date.new(Time.now.year, Time.now.month, -1).day
+    @budget.attack = (@budget.incum - @budget.fixed_cost) / d
+    @resist = 0
+  end
+
 end
