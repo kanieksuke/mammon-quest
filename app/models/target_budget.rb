df10ed232d1f25f477692e79c209da0810e131b2
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-02 21:51:29 +0900

    formオブジェクトを用いてテーブルに保存

--- app/models/target_budget.rb
+++ app/models/target_budget.rb
@@ -1,15 +1,21 @@
 class TargetBudget
   include ActiveModel::Model
-  attr_accessor :target_amount, :target_date, :current_amount, :current_date, :income, :fixed_cost, :attack, :resist, :user_id, :target_id
+  attr_accessor :target_amount, :target_date, :current_amount, :current_date, :income, :fixed_cost, :attack, :resist, :user_id
 
   with_options presence: true do
     validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
     validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
     validates :current_amount, numericality: {only_integer: true}
     validates :current_date, numericality: {only_integer: true}
+    validates :user_id
     validates :income, numericality: {only_integer: true}
     validates :fixed_cost, numericality: {only_integer: true}
     validates :attack, numericality: {only_integer: true}
     validates :resist, numericality: {only_integer: true}
+  end
+
+  def save
+    target = Target.create(target_amount: target_amount, target_date: target_date, user_id: user_id)
+    Budget.create(income: income, fixed_cost: fixed_cost, target_id: target.id)
   end
 end
