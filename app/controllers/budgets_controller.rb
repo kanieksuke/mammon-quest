03bf40ab46f78f1725bff8a1e68138f25e97e722
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-11 16:38:21 +0900

    収支編集確認画面

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,8 +1,23 @@
 class BudgetsController < ApplicationController
+
   def edit
     @budget = Budget.find(params[:id])
   end
 
   def update
+    @target = Target.find(params[:id])
+    @budget = Budget.update(budget_params)
+    create_attack
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit(:income, :fixed_cost).merge(target_id: params[:target_id])
+  end
+
+  def create_attack
+    d = Date.new(Time.now.year, Time.now.month, -1).day
+    @attack = (@target.budget.income - @target.budget.fixed_cost) / d - @target.shopping.resist
   end
 end
