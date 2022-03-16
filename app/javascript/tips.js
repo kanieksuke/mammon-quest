3fd5e6adb64ba6f58f14a5c718a86aa13b9ee7d0
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-16 18:22:11 +0900

    tipsを表示させることに成功!

--- app/javascript/tips.js
+++ app/javascript/tips.js
@@ -2,10 +2,16 @@ function tips () {
   const getAttack = document.getElementById("attack-button");
   getAttack.addEventListener("mouseover", () => {
     getAttack.setAttribute("style", "background-color:gray")
+    const getAttackTips = document.getElementById("tips")
+    var attackTips = "マモンに攻撃をします。一日に一回しか攻撃できないから注意してね！"
+    getAttackTips.innerHTML = `${attackTips}`
   })
 
   getAttack.addEventListener("mouseout", () => {
     getAttack.setAttribute("style", "background-color:black")
+    const deleteAttackTips = document.getElementById("tips")
+    var attackTips = ""
+    deleteAttackTips.innerHTML = `${attackTips}`
   })
 }
 
