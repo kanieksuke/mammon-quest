46441f09d0f4cfab5030c4fda0e2b4d9a10ffaec
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-16 19:18:52 +0900

    収支修正tips作成

--- app/javascript/tips.js
+++ app/javascript/tips.js
@@ -3,7 +3,7 @@ function tips () {
   getAttack.addEventListener("mouseover", () => {
     getAttack.setAttribute("style", "background-color:gray")
     const getAttackTips = document.getElementById("tips")
-    var attackTips = "マモンに攻撃をします。<br>一日に一回しか攻撃できないから注意してね！<br>買い物をし過ぎると逆にマモンのHPが増えちゃうよ！"
+    var attackTips = "マモンに攻撃をします。<br><br>一日に一回しか攻撃できないから注意してね！<br><br>買い物をし過ぎると逆にマモンのHPが増えちゃうよ！"
     getAttackTips.innerHTML = `${attackTips}`
   })
 
@@ -18,7 +18,7 @@ function tips () {
   getShopping.addEventListener("mouseover", () => {
     getShopping.setAttribute("style", "background-color:gray")
     const getShoppingTips = document.getElementById("tips")
-    var shoppingTips = "買い物をしたら金額を入れて、このボタンをクリックしてください。<br>無駄遣いし過ぎると攻撃力が下がるよ！<br>欲しがりません勝つまでは٩( 'ω' )و"
+    var shoppingTips = "買い物をしたら金額を入れて、このボタンをクリックしてください。<br><br>無駄遣いし過ぎると攻撃力が下がるよ！<br><br>欲しがりません勝つまでは٩( 'ω' )و"
     getShoppingTips.innerHTML = `${shoppingTips}`
   })
 
@@ -27,6 +27,21 @@ function tips () {
     const deleteShoppingTips = document.getElementById("tips")
     var shoppingTips = ""
     deleteShoppingTips.innerHTML = `${shoppingTips}`
+  })
+
+  const getEditBudget = document.getElementById("edit-budget-button");
+  getEditBudget.addEventListener("mouseover", () => {
+    getEditBudget.setAttribute("style", "background-color:gray")
+    const getEditBudgetTips = document.getElementById("tips")
+    var editBudgetTips = "初期設定や収支報告で間違えて入力してしまったらここで修正してください。<br><br>※※注意※※すでに与えたダメージには反映されません<br><br>なるたけマモンに攻撃する前に修正してね！"
+    getEditBudgetTips.innerHTML = `${editBudgetTips}`
+  })
+
+  getEditBudget.addEventListener("mouseout", () => {
+    getEditBudget.setAttribute("style", "background-color:black")
+    const deleteEditBudgetTips = document.getElementById("tips")
+    var editBudgetTips = ""
+    deleteEditBudgetTips.innerHTML = `${editBudgetTips}`
   })
 }
 
