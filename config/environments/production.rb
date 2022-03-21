d23ab6b0b7f18a0318f49f1baf92f254838b8be0
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-21 20:47:11 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       :user_name => ENV["SENDGRID_USERNAME"],
       :password => ENV["SENDGRID_PASSWORD"],
-      :domain => "heroku.com",
+      :domain => "herokuapp.com",
       :port => 587,
       :authentication => :plain,
       :evable_starttls_auto => true
