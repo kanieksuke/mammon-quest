abcd83dc5122c8f78f799ae9980ed48347e3327f
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-21 20:09:15 +0900

    本番環境でもメールを送信したい

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mammon_quest_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = {host: 'mammon-quest.herokuapp.com'}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV["GMAIL_USERNAME"],
+      :password => ENV["GMAIL_PASSWORD"],
+      :domain => "heroku.com",
+      :port => 587,
+      :authentication => :plain,
+      :evable_starttls_auto => true
+    }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
