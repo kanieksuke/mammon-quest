c2a329e4a6fcd842b6fba96765b3d5dd527460d5
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-23 20:09:23 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mammon_quest_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: 'mammon-quest.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'https://www.mammon-quest.herokuapp.com'}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
     config.action_mailer.raise_delivery_errors = false
@@ -72,7 +72,8 @@ Rails.application.configure do
       :domain => "heroku.com",
       :port => 587,
       :authentication => :plain,
-      :evable_starttls_auto => true
+      :enable_starttls_auto => true,
+      :address => "smtp.sendgrid.net"
     }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
