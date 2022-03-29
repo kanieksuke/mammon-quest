fe4438f6f94022be3cb241a546ef702e465d3d6a
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-29 20:56:48 +0900

    メッセージは真ん中の方まで一列で表示

--- app/models/target_budget.rb
+++ app/models/target_budget.rb
@@ -4,7 +4,7 @@ class TargetBudget
 
   with_options presence: true do
     validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
-    validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1, message: "目標期日は明日以降に設定してください"}
     validates :current_amount, numericality: {only_integer: true}
     validates :current_date, numericality: {only_integer: true}
     validates :user_id
