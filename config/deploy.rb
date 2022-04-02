5eb9c4266f8ca5b46f4622f68ea39eb8d29f5120
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-04-02 11:59:00 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+lock '3.17.0'
+
+set :application, 'mammon-quest'
+
+set :repo_url, 'git@github.com:kanieksuke/mammon-quest.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/kanieksuke/pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
